19832fd886fd01edab5e8949bacd55b98d0dcc9a
GitHub <noreply@github.com>
2021-10-05 00:16:52 +0000

    :package: Update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as path from "https://deno.land/std@0.109.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.109.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.110.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.110.0/fs/mod.ts";
 
 export type { Disposable } from "https://deno.land/x/disposable@v1.0.1/mod.ts";
