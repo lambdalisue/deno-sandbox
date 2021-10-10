0100846d248703dc38adf007cbf2019b51d9fd29
lambdalisue <lambdalisue@users.noreply.github.com>
2021-10-10 10:39:59 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    ./deps_test.ts
    [1/2] Looking for releases: https://deno.land/std@0.109.0/testing/asserts.ts
    [1/2] Attempting update: https://deno.land/std@0.109.0/testing/asserts.ts -> 0.110.0
    [1/2] Update successful: https://deno.land/std@0.109.0/testing/asserts.ts -> 0.110.0
    [2/2] Looking for releases: https://deno.land/x/disposable@v1.0.1/mod.ts
    [2/2] Attempting update: https://deno.land/x/disposable@v1.0.1/mod.ts -> v1.0.2
    [2/2] Update successful: https://deno.land/x/disposable@v1.0.1/mod.ts -> v1.0.2
    
    ./mod.ts
    
    ./deps.ts
    [1/3] Looking for releases: https://deno.land/std@0.109.0/path/mod.ts
    [1/3] Attempting update: https://deno.land/std@0.109.0/path/mod.ts -> 0.110.0
    [1/3] Update successful: https://deno.land/std@0.109.0/path/mod.ts -> 0.110.0
    [2/3] Looking for releases: https://deno.land/std@0.109.0/fs/mod.ts
    [2/3] Attempting update: https://deno.land/std@0.109.0/fs/mod.ts -> 0.110.0
    [2/3] Update successful: https://deno.land/std@0.109.0/fs/mod.ts -> 0.110.0
    [3/3] Looking for releases: https://deno.land/x/disposable@v1.0.1/mod.ts
    [3/3] Attempting update: https://deno.land/x/disposable@v1.0.1/mod.ts -> v1.0.2
    [3/3] Update successful: https://deno.land/x/disposable@v1.0.1/mod.ts -> v1.0.2
    
    ./sandbox.ts
    
    ./sandbox_test.ts
    
    Successfully updated:
    https://deno.land/std@0.109.0/testing/asserts.ts 0.109.0 -> 0.110.0
    https://deno.land/x/disposable@v1.0.1/mod.ts v1.0.1 -> v1.0.2
    https://deno.land/std@0.109.0/path/mod.ts 0.109.0 -> 0.110.0
    https://deno.land/std@0.109.0/fs/mod.ts 0.109.0 -> 0.110.0
    https://deno.land/x/disposable@v1.0.1/mod.ts v1.0.1 -> v1.0.2
    make[1]: Entering directory '/home/runner/work/deno-sandbox/deno-sandbox'
    make[1]: Leaving directory '/home/runner/work/deno-sandbox/deno-sandbox'

--- deps_test.ts
+++ deps_test.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.109.0/testing/asserts.ts";
-export { using } from "https://deno.land/x/disposable@v1.0.1/mod.ts";
+export * from "https://deno.land/std@0.110.0/testing/asserts.ts";
+export { using } from "https://deno.land/x/disposable@v1.0.2/mod.ts";
