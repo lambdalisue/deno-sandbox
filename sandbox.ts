87bc62d2002ec92d1aab73c0ac0f7df26ac44be0
Alisue <lambdalisue@gmail.com>
2023-05-08 09:48:00 +0900

    Upgrade deps

--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,6 @@
-import type { Disposable } from "https://deno.land/x/disposable@v1.1.0/mod.ts";
-import * as path from "https://deno.land/std@0.164.0/path/mod.ts";
-import * as fs from "https://deno.land/std@0.164.0/fs/mod.ts";
+import type { Disposable } from "https://deno.land/x/disposable@v1.1.1/mod.ts";
+import * as path from "https://deno.land/std@0.186.0/path/mod.ts";
+import * as fs from "https://deno.land/std@0.186.0/fs/mod.ts";
 
 export type SandboxOptions = Deno.MakeTempOptions;
 
--- sandbox_test.ts
+++ sandbox_test.ts
@@ -2,10 +2,10 @@ import {
   assert,
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.164.0/testing/asserts.ts";
-import * as path from "https://deno.land/std@0.164.0/path/mod.ts";
-import * as fs from "https://deno.land/std@0.164.0/fs/mod.ts";
-import { using } from "https://deno.land/x/disposable@v1.1.0/mod.ts";
+} from "https://deno.land/std@0.186.0/testing/asserts.ts";
+import * as path from "https://deno.land/std@0.186.0/path/mod.ts";
+import * as fs from "https://deno.land/std@0.186.0/fs/mod.ts";
+import { using } from "https://deno.land/x/disposable@v1.1.1/mod.ts";
 import { sandbox } from "./sandbox.ts";
 
 Deno.test({
